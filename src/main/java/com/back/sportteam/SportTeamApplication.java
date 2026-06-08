a82d1437a53c0e6e16c2eec5404b782f5333a787
5s-mini <dnclsehd123@naver.com>
2026-06-08 11:38:20 +0900

    프로젝트 파일 생성

---
+++ src/main/java/com/back/sportteam/SportTeamApplication.java
@@ -0,0 +1,13 @@
+package com.back.sportteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportTeamApplication.class, args);
+    }
+
+}
