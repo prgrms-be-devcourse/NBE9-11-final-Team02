d8a491525977737274fcc463eb77bfe10cae9821
5s-mini <dnclsehd123@naver.com>
2026-06-10 16:06:47 +0900

    feat: 사용자 인증 도메인 추가

---
+++ src/main/java/com/back/sportteam/user/domain/UserRole.java
@@ -0,0 +1,6 @@
+package com.back.sportteam.user.domain;
+
+public enum UserRole {
+    USER,
+    MANAGER
+}
---
+++ src/main/java/com/back/sportteam/user/reposiroty/UserRepository.java
@@ -0,0 +1,15 @@
+package com.back.sportteam.user.repository;
+
+import com.back.sportteam.auth.domain.AuthProvider;
+import com.back.sportteam.user.domain.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
+}
