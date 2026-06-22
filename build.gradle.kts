618c5e421aa99ad48eca16e022afc1326a39d53b
5s-mini <dnclsehd123@naver.com>
2026-06-22 14:36:40 +0900

    test: FacilityQueryRepositoryTest 테스트 코드 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.redisson:redisson:$redissonVersion")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+    testImplementation("org.springframework.boot:spring-boot-data-jpa-test")
 
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
