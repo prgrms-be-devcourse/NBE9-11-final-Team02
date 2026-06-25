429ecf590c8eee8e996e9ff0936b25c0b6a65a77
k-equals0 <sallykey@naver.com>
2026-06-25 11:29:39 +0900

    test: S3 Presigned URL 및 시설 이미지 삭제 테스트 추가

---
+++ src/test/java/com/back/sportteam/infra/s3/S3ServiceTest.java
@@ -0,0 +1,79 @@
+package com.back.sportteam.infra.s3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+@ExtendWith(MockitoExtension.class)
+class S3ServiceTest {
+
+    private static final String BUCKET = "test-bucket";
+    private static final String REGION = "ap-northeast-2";
+    private static final int EXPIRATION = 10;
+
+    @Mock
+    private S3Client s3Client;
+
+    @Mock
+    private S3Presigner s3Presigner;
+
+    @Mock
+    private PresignedPutObjectRequest presignedPutObjectRequest;
+
+    private S3Service s3Service;
+
+    @BeforeEach
+    void setUp() {
+        S3Properties properties = new S3Properties(BUCKET, REGION, EXPIRATION);
+        s3Service = new S3Service(s3Client, s3Presigner, properties);
+    }
+
+    @Test
+    void presignedUrl_발급_시_uploadUrl과_fileUrl을_반환한다() throws MalformedURLException {
+        when(s3Presigner.presignPutObject(any(PutObjectPresignRequest.class)))
+                .thenReturn(presignedPutObjectRequest);
+        when(presignedPutObjectRequest.url())
+                .thenReturn(new URL("https://test-bucket.s3.ap-northeast-2.amazonaws.com/facilities/uuid?X-Amz=sig"));
+
+        PresignedUrlResponse response = s3Service.generatePresignedUrl("image/jpeg");
+
+        assertThat(response.uploadUrl()).contains("X-Amz=sig");
+        assertThat(response.fileUrl()).startsWith("https://test-bucket.s3.ap-northeast-2.amazonaws.com/facilities/");
+    }
+
+    @Test
+    void 버킷_파일_삭제_시_올바른_key로_S3에_요청한다() {
+        String fileUrl = "https://test-bucket.s3.ap-northeast-2.amazonaws.com/facilities/some-uuid";
+
+        s3Service.deleteFile(fileUrl);
+
+        ArgumentCaptor<DeleteObjectRequest> captor = ArgumentCaptor.forClass(DeleteObjectRequest.class);
+        verify(s3Client).deleteObject(captor.capture());
+        assertThat(captor.getValue().key()).isEqualTo("facilities/some-uuid");
+        assertThat(captor.getValue().bucket()).isEqualTo(BUCKET);
+    }
+
+    @Test
+    void 다른_버킷의_URL로_삭제_요청하면_예외가_발생한다() {
+        String wrongUrl = "https://other-bucket.s3.ap-northeast-2.amazonaws.com/facilities/uuid";
+
+        assertThatThrownBy(() -> s3Service.deleteFile(wrongUrl))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
